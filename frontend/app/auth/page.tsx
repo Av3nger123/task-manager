25672377c98672c8870583bfc1a056be472eac49
Anyam Sai Praneeth Reddy <praneeth.reddy7984@gmail.com>
2025-05-11 18:19:09 +0530

    auto nav based on session

--- frontend/app/auth/page.tsx
+++ frontend/app/auth/page.tsx
@@ -1,8 +1,21 @@
 "use client";
 
 import { LoginForm } from "@/components/login-form";
+import { useAuth } from "@/lib/context/auth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
+	const { isAuthenticated } = useAuth();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (isAuthenticated) {
+			router.push("/");
+			return;
+		}
+	}, [isAuthenticated, router]);
+
 	return (
 		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
 			<div className="w-full max-w-sm">
